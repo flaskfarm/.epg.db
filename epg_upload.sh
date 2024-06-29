a2fd9c2daf126d844691c490df6a867cba7da184
soju6jan <cybersol@naver.com>
2024-06-30 06:09:57 +0900

    20240630-0609

--- epg_upload.sh
+++ epg_upload.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 export LANG=en_US.utf8
+
 NowDate=$(date +%Y%m%d)-$(date +%H%M)
 PLUGIN_HOME=$1
 cd $PLUGIN_HOME; 
+git config --global user.name "soju6jan"
+git config --global user.email "cybersol@naver.com"
+git remote set-url origin git@github.com:flaskfarm/.epg.db.git
 git --git-dir=$PLUGIN_HOME/.git pull;
+git --git-dir=$PLUGIN_HOME/.git add $PLUGIN_HOME/epg_upload.sh;
 git --git-dir=$PLUGIN_HOME/.git add $PLUGIN_HOME/epg_data.db;
 git --git-dir=$PLUGIN_HOME/.git add $PLUGIN_HOME/xmltv.xml;
 git --git-dir=$PLUGIN_HOME/.git add $PLUGIN_HOME/UPDATED_TIME;
